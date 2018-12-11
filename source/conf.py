9e5070ce8cbb0dcfaf4f3cbca03ed2628ae574fa
Yasuki Ikeuchi <ikeyasu@gmail.com>
2018-12-11 23:45:04 +0900

    modify to bootstrap/united

--- source/conf.py
+++ source/conf.py
@@ -17,9 +17,10 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+import sphinx_bootstrap_theme
 
 
 # -- General configuration ------------------------------------------------
@@ -85,7 +86,9 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'bootstrap'
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+html_theme_options = {'bootswatch_theme': "united"}
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
